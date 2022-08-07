467bf841cbc156cd17f0c7187f1bd14a44ebfd4e
zar4za <zar4z4@yandex.ru>
2022-08-07 15:26:25 +0300

    Проект перенесен на MVC, настроены параметры dev запуска

--- web/src/Controllers/SampleController.cs
+++ web/src/Controllers/SampleController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
@@ -7,6 +6,7 @@ namespace Web.Controllers
     [ApiController]
     public class SampleController : ControllerBase
     {
+        [HttpGet]
         public string Get()
         {
             return "Hello, wolrd!";
--- web/src/Program.cs
+++ web/src/Program.cs
@@ -1,7 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMvc();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
