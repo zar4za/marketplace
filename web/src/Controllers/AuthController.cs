cdbfbc638e66821877513d836935d002df3659ec
zar4za <zar4z4@yandex.ru>
2022-08-10 01:21:43 +0300

    Jwt токены работают, но нужно прибраться в коде

---
+++ web/src/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Web.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        [HttpGet("get")]
+        public async Task<IActionResult> Get()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("steamid", "123")
+            };
+
+
+            var jwt = new JwtSecurityToken(
+                    claims: claims,
+                    audience: "localhost",
+                    expires: DateTimeOffset.UtcNow.AddMinutes(240).UtcDateTime,
+                    signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes("supersecretpassword")), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return Ok(encodedJwt);
+        }
+
+        [Authorize]
+        [HttpGet("check")]
+        public async Task<IActionResult> Check()
+        {
+            return Ok(new { message = "success" });
+        }
+    }
+}
--- web/src/Program.cs
+++ web/src/Program.cs
@@ -1,13 +1,40 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.Audience = "localhost";
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+        {
+            ValidateIssuer = false,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("supersecretpassword"))
+        };
+    });
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.AllowAnyOrigin();
+            builder.AllowAnyHeader();
+            builder.AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
+app.UseAuthentication();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseRouting();
+app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
